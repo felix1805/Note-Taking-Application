b58b0198e1da6a0d43e6a4916b49a0517c421193
felix1805 <felix1805@gmail.com>
2022-03-24 18:28:42 -0400

    began api call config for notes section.

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.use(express.urlencoded({extended:true}));
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+app.get('/api/notes', (req, res)=> {
+    const data = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+    const notesJson = JSON.parse(data);
+    res.json(notesJson);
+
+});
 
 
 app.get('*',(req, res)=>{
