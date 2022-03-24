84fb0b87448a61063e407c840cd1296f38481edd
felix1805 <felix1805@gmail.com>
2022-03-24 17:55:13 -0400

    linked notes.html file to server.js using app.get

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
+const dbJson = require('./db/db.json')
 
+
+const uuid = require('./helpers/uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -8,9 +12,14 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+
 app.get('*',(req, res)=>{
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-})
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
