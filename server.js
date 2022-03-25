03d2fd2d09df3cc9511f23909d800e92ad2ec1c7
felix1805 <felix1805@gmail.com>
2022-03-24 20:33:05 -0400

    completed server.js storing values in db.json

--- server.js
+++ server.js
@@ -4,28 +4,39 @@ const path = require('path');
 const dbJson = require('./db/db.json')
 
 
-const uuid = require('./helpers/uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const uuid = require('./helpers/uuid');
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/notes', (req, res) => {
+app.get('/notes', (req, res)=>{
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-app.get('/api/notes', (req, res)=> {
-    const data = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+app.get('/api/notes', (req, res)=>{
+    const data = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf-8');
     const notesJson = JSON.parse(data);
     res.json(notesJson);
+    req.body.id = uuid();
 
+});
+app.post('/api/notes', (req, res)=>{
+    const data = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf-8');
+    const notesJson = JSON.parse(data);
+    req.body.id = uuid();
+    notesJson.push(req.body);
+
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notesJson), 'utf-8');
+    res.json();
 });
 
 
-app.get('*',(req, res)=>{
+app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, './public/index.html'));
+    req.body.id = uuid();
 });
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+app.listen(PORT, ()=>
+    console.log(`Example app listening at http://localhost:${PORT}`)
 );
